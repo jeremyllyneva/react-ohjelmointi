3c725f9bf209e1dd96af622645eeca0fed203ef0
jeremyllyneva <109760921+jeremyllyneva@users.noreply.github.com>
2023-08-21 13:03:43 +0300

    Create Course.jsx

---
+++ osa1/kurssitiedot/2.5: kurssitiedot/Course.jsx
@@ -0,0 +1,15 @@
+import Header from './Header'
+import Content from './Content'
+import Total from './Total'
+
+const Course = ({ name, parts }) => {
+  return (
+    <div>
+      <Header course={name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default Course
