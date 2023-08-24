8bdcb7621ebcdfc11a7dd45fee5a5a8a32007611
jeremyllyneva <109760921+jeremyllyneva@users.noreply.github.com>
2023-08-24 09:04:48 +0300

    Update PersonForm.jsx

--- osa2/puhelinluettelo/2.10: puhelinluettelo/components/PersonForm.jsx
+++ osa2/puhelinluettelo/2.10: puhelinluettelo/components/PersonForm.jsx
@@ -1,20 +1,22 @@
 const PersonForm = ({ addPerson, handleNameChange, handleNumberChange, newName, newNumber}) =>
-<form onSubmit={addPerson}>
-    <div>
-        name: <input
-            value={newName}
-            onChange={handleNameChange}
-        />
-    </div>
-    <div>
-        number: <input
-            value={newNumber}
-            onChange={handleNumberChange}
-        />
-    </div>
-    <div>
-        <button type="submit">add</button>
-    </div>
-</form>
+    return (
+        <form onSubmit={addPerson}>
+            <div>
+                name: <input
+                    value={newName}
+                    onChange={handleNameChange}
+                />
+            </div>
+            <div>
+                number: <input
+                    value={newNumber}
+                    onChange={handleNumberChange}
+                />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
 
 export default PersonForm
